ae5a6a8c11c6f1d44b252340427ed186694e0041
Chris Hapgood <cch1@hapgoods.com>
2020-07-30 23:55:15 -0400

    Add btc.finite-field/element? predicate

--- src/btc/finite_field.clj
+++ src/btc/finite_field.clj
@@ -1,5 +1,5 @@
 (ns btc.finite-field
-  "Follow along for https://btcclj.com/")
+  "Follow along for https://btcclj.com/, but with clarity and idiomatic Clojure")
 
 (defn- modpow [base exponent modulus]
   (loop [result 1 exponent exponent]
@@ -35,28 +35,35 @@
   (let [as (rand-ints p (min p 50))]
     (transduce (map (partial fermat-prime? p)) and-as-fn as)))
 
+(defn element?
+  "Is the given element `element` a member of the finite field of order `p`?"
+  [p x]
+  {:pre [(integer? p) (integer? x)]}
+  (and (<= 0 x) (< x p)))
+
+;; Define operations applicable to _prime_ fields
 (defn +f
   ([p] 0) ; nullary returns additive identity
   ([p n] (+f p 0 n))
-  ([p n m] {:pre [(<= 0 n) (< n p) (<= 0 m) (< m p) (prime? p)]} (mod (+ n m) p))
+  ([p n m] {:pre [(element? p n) (element? p m) (prime? p)]} (mod (+ n m) p))
   ([p n m & ms] (reduce (partial +f p) (+f p n m) ms)))
 
 (defn -f
   ([p n] (-f p 0 n)) ; unary returns the additive inverse of n
-  ([p n m] {:pre [(<= 0 n) (< n p) (<= 0 m) (< m p) (prime? p)]} (mod (- n m) p))
+  ([p n m] {:pre [(element? p n) (element? p m) (prime? p)]} (mod (- n m) p))
   ([p n m & ms] (reduce (partial -f p) (-f p n m) ms)))
 
 (defn *f
   ([p] 1) ; nullary returns multiplicative identity
   ([p n]  (*f p 1 n))
-  ([p n m] {:pre [(<= 0 n) (< n p) (<= 0 m) (< m p) (prime? p)]} (mod (* n m) p))
+  ([p n m] {:pre [(element? p n) (element? p m) (prime? p)]} (mod (* n m) p))
   ([p n m & ms] (reduce (partial *f p) (*f p n m) ms)))
 
 (defn divf ; ⧸ ∕
   ([p n] (divf p 1 n)) ; unary returns the multiplicative inverse of n
-  ([p n m] {:pre [(<= 0 n) (< n p) (<= 0 m) (< m p) (prime? p)]}
+  ([p n m] {:pre [(element? p n) (element? p m) (prime? p)]}
    (let [m' (modpow m (- p 2) p)] ; from Fermat's Little Theorem
      (*f p n m')))
   ([p n m & ms] (reduce (partial divf p) (divf p n m) ms)))
 
-(defn **f [p n k] (modpow n k p))
+(defn **f [p n k] {:pre [(element? p n) (prime? p)]} (modpow n k p))
