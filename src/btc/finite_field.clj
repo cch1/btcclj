d1be3fe064264c2d551ff9714a1b1f914874ffab
Chris Hapgood <cch1@hapgoods.com>
2020-07-30 21:18:29 -0400

    Use idiomatic Clojure
    We don't need no stinking custom types.

--- src/btc/finite_field.clj
+++ src/btc/finite_field.clj
@@ -1,8 +1,6 @@
 (ns btc.finite-field
   "Follow along for https://btcclj.com/")
 
-(defrecord FieldElement [e p])
-
 (defn- modpow [base exponent modulus]
   (loop [result 1 exponent exponent]
     (if (zero? exponent)
@@ -11,7 +9,7 @@
 
 (defn- fermat-prime?
   "Does the given integer `p` pass the Fermat test for primality against the given `a`?"
-  ;; NB: This test does not distinguish between primes and Charmichael numbers, or psuedoprimes.
+  ;; NB: This test does not distinguish between primes and Charmichael numbers, or psuedoprimes as a consequence of using FLT
   [p a]
   {:pre [(<= 0 a (dec p))]}
   (= (modpow a p p) a))
@@ -37,34 +35,28 @@
   (let [as (rand-ints p (min p 50))]
     (transduce (map (partial fermat-prime? p)) and-as-fn as)))
 
-(defn field-element [e p]
-  {:pre [(<= 0 e) (< e p) (prime? p)]}
-  (->FieldElement e p))
+(defn +f
+  ([p] 0) ; nullary returns additive identity
+  ([p n] (+f p 0 n))
+  ([p n m] {:pre [(<= 0 n) (< n p) (<= 0 m) (< m p) (prime? p)]} (mod (+ n m) p))
+  ([p n m & ms] (reduce (partial +f p) (+f p n m) ms)))
 
-(defprotocol FieldOperations
-  (+f [x y])
-  (-f [x y])
-  (*f [x y])
-  (divf  [x y])
-  (**f [x k]))
+(defn -f
+  ([p n] (-f p 0 n)) ; unary returns the additive inverse of n
+  ([p n m] {:pre [(<= 0 n) (< n p) (<= 0 m) (< m p) (prime? p)]} (mod (- n m) p))
+  ([p n m & ms] (reduce (partial -f p) (-f p n m) ms)))
 
-(defn assert-same-prime-order [{e0 :e p0 :p :as fe0} {e1 :e p1 :p :as fe1}]
-  (assert (= p0 p1) "Field elementss need to be of the same prime order"))
+(defn *f
+  ([p] 1) ; nullary returns multiplicative identity
+  ([p n]  (*f p 1 n))
+  ([p n m] {:pre [(<= 0 n) (< n p) (<= 0 m) (< m p) (prime? p)]} (mod (* n m) p))
+  ([p n m & ms] (reduce (partial *f p) (*f p n m) ms)))
 
-(extend-type FieldElement
-  FieldOperations
-  (+f [{e0 :e p0 :p :as fe0} {e1 :e p1 :p :as fe1}]
-    (assert-same-prime-order fe0 fe1)
-    (field-element (mod (+ e0 e1) p0) p0))
-  (-f [{e0 :e p0 :p :as fe0} {e1 :e p1 :p :as fe1}]
-    (assert-same-prime-order fe0 fe1)
-    (field-element (mod (- e0 e1) p0) p0))
-  (*f [{e0 :e p0 :p :as fe0} {e1 :e p1 :p :as fe1}]
-    (assert-same-prime-order fe0 fe1)
-    (field-element (mod (* e0 e1) p0) p0))
-  (divf [fe0 {e1 :e p1 :p :as fe1}]
-    (let [inverse-e1 (modpow e1 (- p1 2) p1) ; from Fermat's Little Theorem
-          inverse-fe1 (field-element inverse-e1 p1)]
-      (*f fe0 inverse-fe1)))
-  (**f [{e :e p :p :as fe} k]
-    (field-element (modpow e k p) p)))
+(defn divf ; ⧸ ∕
+  ([p n] (divf p 1 n)) ; unary returns the multiplicative inverse of n
+  ([p n m] {:pre [(<= 0 n) (< n p) (<= 0 m) (< m p) (prime? p)]}
+   (let [m' (modpow m (- p 2) p)] ; from Fermat's Little Theorem
+     (*f p n m')))
+  ([p n m & ms] (reduce (partial divf p) (divf p n m) ms)))
+
+(defn **f [p n k] (modpow n k p))
